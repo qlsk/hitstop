e9e78b326e783e4c33717092aea803ad702a34a8
qlsk <jhanggu0625@gmail.com>
2026-09-06 21:56:54 +0900

    feat: 플레이어 이동 기능 구현

---
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] [Header("캐릭터 이동 속도")] private float _moveSpeed;
+    [SerializeField] [Header("점프력")] private float _jumpPower;
+    public Rigidbody2D _rigidbody;
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Time.deltaTime * _moveSpeed * Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Time.deltaTime * _moveSpeed * Vector3.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _jumpPower);
+        }
+    }
+}
