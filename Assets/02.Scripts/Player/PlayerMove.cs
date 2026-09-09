44ba6f41408d8063615322cf5df8b51a55549cbd
qlsk <jhanggu0625@gmail.com>
2026-09-09 18:23:27 +0900

    feat: 점프 후 최대 높이에서 떨어지는 속도 증가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -6,8 +6,11 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] [Header("캐릭터 이동 속도")] private float _moveSpeed;
     [SerializeField] [Header("점프력")] private float _jumpPower;
+    [SerializeField] [Header("하강시 중력 값")] private float _downSpeed;
     public Rigidbody2D _rigidbody;
     private bool _isGrounded;
+    private bool _isJumping;
+    private float _jumpTimer;
 
     private void Start()
     {
@@ -27,8 +30,24 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _jumpPower);
+            Jump();
         }
+
+        if (_isJumping)
+        {
+            _jumpTimer += Time.deltaTime;
+            if (_rigidbody.linearVelocityY < 0)
+            {
+                _isJumping = false;
+                _rigidbody.gravityScale = _downSpeed;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(_jumpPower * Vector2.up, ForceMode2D.Impulse);
+        _isJumping = true;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -36,6 +55,8 @@ public class PlayerMove : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             _isGrounded = true;
+            _rigidbody.gravityScale = 1f;
+            _jumpTimer = 0;
         }
     }
 
